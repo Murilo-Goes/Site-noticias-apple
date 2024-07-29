9e0617dff10dda611a9417dcbde7078d165754a3
Murilo Góes <murilogoes25@gmail.com>
2024-07-29 11:22:41 -0300

    Sites Noticias Apple

---
+++ _javascript/funcoes.js
@@ -0,0 +1,11 @@
+
+
+  // função que chama a função mudaicone() quando o botão é clicado
+  function mudaicone (foto) {
+    document.getElementById("icone").src = foto ;     // estamos mexendo no iphone 3gs 
+
+  }
+
+
+
+
